681fbd0df9ecf9421c59c0e10f18c443c92c52ce
sbrycbc <sabriyecbc@gmail.com>
2023-08-09 21:32:22 +0200

    added weather.js with CLI application...

---
+++ node-weather/weather.js
@@ -0,0 +1,9 @@
+//------- Weather CLI --------//
+// Get the latest weather report - from your terminal!
+
+//------- What you will be doing --------//
+
+// You will be writing a CLI application which uses an external API to get weather information.
+
+// The user will provide a city name when they run the program. Your program must use that city name in its request - and print the results into the terminal.
+
